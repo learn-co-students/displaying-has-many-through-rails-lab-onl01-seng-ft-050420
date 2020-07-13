82fcf86bbd713a2ad976e0e33a841eebcd74390a
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-13 18:13:26 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,8 @@
+class Appointment < ApplicationRecord
+  belongs_to :patient
+  belongs_to :doctor
+
+  def display_date_and_time
+    self.appointment_datetime.strftime("%B %d, %Y") + " at " + self.appointment_datetime.strftime("%H:%M")
+  end
+end
