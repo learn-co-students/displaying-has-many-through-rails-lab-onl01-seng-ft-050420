c815926c52b0a577af4c5337f34c7e532c8b9131
kikuishimoto <kikuish@gmail.com>
2020-07-21 13:52:54 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :appointments
+  resources :d_patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
